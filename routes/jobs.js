66ddf392af94d8b45c9c2283a879362d590ab068
Nezumo <julian2006@freenet.de>
2024-01-17 16:54:15 +0100

    fix

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllJobs,
-        getJob,
-        createJob,
-        updateJob,
-        deleteJob} = require("../controllers/main")
+const { 
+    getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob,
+} = require("../controllers/jobs")
 
 router.route("/").post(createJob).get(getAllJobs)
 router.route("/:id").get(getJob).delete(deleteJob).patch(updateJob)
 
-module.export = router
+module.exports = router
